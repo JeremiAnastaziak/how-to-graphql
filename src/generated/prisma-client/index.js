00f5f7087440f9c1aad09d8506c9670907008bbf
Jeremi Anastaziak <jeremi.anastaziak@allegro.pl>
2019-02-02 15:06:29 +0100

    Add db, use prisma

---
+++ src/generated/prisma-client/index.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var prisma_lib_1 = require("prisma-client-lib");
+var typeDefs = require("./prisma-schema").typeDefs;
+
+var models = [
+  {
+    name: "Link",
+    embedded: false
+  }
+];
+exports.Prisma = prisma_lib_1.makePrismaClientClass({
+  typeDefs,
+  models,
+  endpoint: `https://eu1.prisma.sh/jeremi-anastaziak-3ba3de/how-to-graphql/dev`
+});
+exports.prisma = new exports.Prisma();
