01d7e428fec65fc6653eab81dd96a28e53e10a48
Jeremi Anastaziak <jeremi.anastaziak@allegro.pl>
2019-02-07 17:44:41 +0100

    Add user authentication, structure resolvers

--- src/index.js
+++ src/index.js
@@ -1,35 +1,26 @@
 const { GraphQLServer } = require('graphql-yoga')
 const { prisma } = require('./generated/prisma-client')
 
+const Query = require('./resolvers/Query')
+const Mutation = require('./resolvers/Mutation')
+const User = require('./resolvers/User')
+const Link = require('./resolvers/Link')
+
 const resolvers = {
-  Query: {
-    info: () => `This is the API of a Hackernews Clone`,
-    feed: (root, args, context, info) => {
-      return context.prisma.links()
-    },
-    link: (id, args, context) => {
-      return context.prisma.links()
-    }
-  },
-  Mutation: {
-    post: (parent, args, context) => {
-      return context.prisma.createLink({
-        url: args.url,
-        description: args.description,
-      })
-    },
-    updateLink: (parent, args, context) => {
-      return context.prisma.updateLink({
-        description: args.description,
-        url: args.url,
-      }, { id: args.id })
-    }
-  }
+  Query,
+  Mutation,
+  User,
+  Link,
 }
 
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
   resolvers,
-  context: { prisma }
+  context: request => {
+    return {
+      ...request,
+      prisma,
+    }
+  },
 })
 server.start(() => console.log(`Server is running on http://localhost:4000`))
